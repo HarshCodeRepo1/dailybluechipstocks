90087a46f65bca19467b75842a448e37c9576d4d
Harsh Pandey <harsh98185@gmail.com>
2026-08-10 21:44:15 -0700

    Use dynamic Cognito redirect URI

--- src/auth.ts
+++ src/auth.ts
@@ -3,7 +3,7 @@ import type { AuthProviderProps } from 'react-oidc-context'
 export const cognitoAuthConfig: AuthProviderProps = {
   authority: 'https://cognito-idp.us-east-1.amazonaws.com/us-east-1_gfVsnIELk',
   client_id: '71osdnkvspv2fgoeirmm6fmee6',
-  redirect_uri: 'http://localhost:5173/',
+  redirect_uri: `${window.location.origin}/`,
   response_type: 'code',
   scope: 'openid email',
   onSigninCallback: () => {
